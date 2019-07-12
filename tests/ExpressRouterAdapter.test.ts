c8741b8c0c977f9bd49adb6f02764e9ee5d36f1e
Jonathan Hoguet <jon@symbioticlabs.io>
2019-07-12 15:38:48 -0400

    stubbed out an express test

--- tests/ExpressRouterAdapter.test.ts
+++ tests/ExpressRouterAdapter.test.ts
@@ -1,13 +1,25 @@
 import { ExpressRouterAdapter } from '../src';
+import * as request from 'supertest';
+import * as express from 'express';
 
 // tslint:disable max-classes-per-file
-const mockContainer = { get(): any { return null; } };
-const mockSecurityContextProvider = { async getSecurityContext(): Promise<any> { return null; } };
+// const mockContainer = { get(): any { return null; } };
+// const mockSecurityContextProvider = { async getSecurityContext(): Promise<any> { return null; } };
 
 describe('ExpressRouterAdapter', () => {
 
-  it('Can be constructed', () => {
-    const sut = new ExpressRouterAdapter(mockContainer as any, mockSecurityContextProvider);
+  it('Can be constructed', async () => {
+    // const sut = new ExpressRouterAdapter(mockContainer as any, mockSecurityContextProvider);
+
+    const app = express().use((req, res) => {
+      res.status(200).send();
+    });
+
+    await request(app)
+      .get('/')
+      .expect(200)
+      .then();
+
   });
 
 });
