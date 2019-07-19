80024ad79b5a82550b55e01c42073e0f79745c71
Ben Glassman <ben@symbioticlabs.io>
2019-07-19 12:20:55 -0400

    Tests should use supertest

--- sample-app/integration-tests/pets.spec.ts
+++ sample-app/integration-tests/pets.spec.ts
@@ -1,17 +1,19 @@
 import * as assert from 'assert';
-import axios from 'axios';
+import * as request from 'supertest';
 
 describe('pets', () => {
-  let http;
-
-  before(() => {
-    http = axios.create({ baseURL :  'http://localhost:4000', headers: { authorization: 'not-real-user', accept: 'application/json'} });
-  });
+  const baseURL = 'http://localhost:4000';
 
   it('I can get pets', async () => {
-    const { data, status } = await http.get('/pets', { headers: { accept: 'application/pets+json'}});
-
-    assert.equal(status, 200);
-    assert.deepEqual(data, [{ firstName: 'honey', lastName: 'hoguet'}, { firstName: 'poseidon', lastName: 'hoguet'}]);
+    await request(baseURL)
+      .get('/pets')
+      .set('authorization', 'not-real-user')
+      .set('accept', 'application/pets+json')
+      .expect(200)
+      .expect([
+        { firstName: 'honey', lastName: 'hoguet' },
+        { firstName: 'poseidon', lastName: 'hoguet' }
+      ]);
   });
+
 });
